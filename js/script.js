e679ac9c936d22489534afe418e1e9ea0f8556db
user 16 <user16@or-e4.perm.21-school.ru>
2026-06-06 13:19:37 +0500

    Create emergency page.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function onEmergency() {
+    let emergencyPage = document.getElementById("overlay-page");
+    emergencyPage.style.display = 'flex';
+}
+function onEmergencyCancel() {
+    let emergencyPage = document.getElementById("overlay-page");
+    emergencyPage.style.display = 'none';
+}
